65ca62b95ff211fd71a7940ee51884a4fcaf8820
unknown <boris.galac@gmail.com>
2023-09-17 01:49:54 +0200

    update

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// HAMBURGER BTN AND MENU
+
 const hamBtn = document.querySelector(".ham-btn");
 const navList = document.querySelector(".nav__list");
 const hamLine = document.querySelectorAll(".ham-line");
@@ -15,4 +17,29 @@ hamBtn.addEventListener("click", (e) => {
       line.setAttribute("aria-expanded", "false");
     });
   }
+});
+
+// AD AUTOMATIC SLIDER
+
+const carousel = document.querySelector(".ads");
+const innerWrapper = document.querySelector(".ads__slider");
+let direction;
+
+let slideInterval = setInterval(nextSlide, 3000);
+
+function nextSlide() {
+  direction = -1;
+  innerWrapper.style.transition = "transform 1.5s ease-in-out";
+  innerWrapper.style.transform = "translateX(-33.33%)";
+}
+
+innerWrapper.addEventListener("transitionend", (e) => {
+  if (direction === -1) {
+    innerWrapper.appendChild(innerWrapper.firstElementChild);
+  } else if (direction === 1) {
+    innerWrapper.prepend(innerWrapper.lastElementChild);
+  }
+
+  innerWrapper.style.transition = "none";
+  innerWrapper.style.transform = "translateX(0)";
 });
