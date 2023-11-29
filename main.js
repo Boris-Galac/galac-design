cae1bca7c5a0c9272f0ea854e24793bf15bf221e
unknown <boris.galac@gmail.com>
2023-11-29 12:18:04 +0100

    update

--- main.js
+++ main.js
@@ -571,3 +571,23 @@ function createOverlay(element) {
     });
   });
 }
+
+// PAGE TRANSITIONS
+
+window.addEventListener("load", (e) => {
+  const transitionElement = document.querySelector(".transition");
+  const allLinksOnPage = document.querySelectorAll(".nav__link--trans");
+  setTimeout(() => {
+    transitionElement.setAttribute("data-active", "false");
+  }, 500);
+  allLinksOnPage.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      const target = e.target.href;
+      transitionElement.setAttribute("data-active", "true");
+      setTimeout(() => {
+        window.location.href = target;
+      }, 500);
+    });
+  });
+});
